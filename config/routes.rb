945bca330c0712affad3a6a9db82e1a73df2a2be
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-27 14:26:28 +0300

    attempt one

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  resources :movies, only: [:index]
+  resources :movies, only: [:index, :create]
+end
+Then, add a controller method:
+
+class MoviesController < ApplicationController
+
+  def create
+    byebug
+    
+  end
+
+  def index
+    movies = Movie.all
+    render json: movies
+  end
+
 end
